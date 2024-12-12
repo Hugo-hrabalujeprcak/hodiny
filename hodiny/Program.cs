ce85746d515e6899594e6edd085de3d30501fc3a
h.hrabal <h.hrabal@PC-IVT11>
2024-12-12 17:24:36 +0900

    Přidejte soubory projektu.

---
+++ hodiny/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+while (1 + 1 == 2)
+{
+    DateTime cas = DateTime.Now;
+    if (cas.Hour == 12)
+        Console.WriteLine("cas na pivco");
+    Console.WriteLine(cas);
+    Console.WriteLine(DateTime.Now.DayOfWeek);
+    Thread.Sleep(1000);
+    Console.Clear();
+}
